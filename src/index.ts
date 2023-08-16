9673f9c8c5251f055f9d69bef98d4c839dca4eb0
Ajay Nair A <ajaynairarun@gmail.com>
2023-08-16 12:16:21 +0530

    add : bfs method

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import Graph from "./representation/adjacency-matrix/non-directed";
 
-const graph = new Graph(5);
+const graph = new Graph(4);
+graph.addEdge(0, 1);
+graph.addEdge(0, 2);
+graph.addEdge(1, 2);
+graph.addEdge(2, 0);
+graph.addEdge(2, 3);
+graph.addEdge(3, 3);
 console.log(graph);
+graph.bfs(2);
--- src/representation/adjacency-matrix/non-directed/index.ts
+++ src/representation/adjacency-matrix/non-directed/index.ts
@@ -4,10 +4,36 @@ class Graph {
 
 	constructor(numberOfNodes: number) {
 		this.numberOfNodes = numberOfNodes;
-		this.matrix = new Array(numberOfNodes).fill([]);
+		this.matrix = new Array(numberOfNodes).fill(null).map(() => []);
 	}
 
-	public addEdge = (source: number, destination: number, weight: number) => {};
+	public addEdge(source: number, destination: number) {
+		this.matrix[source].push(destination);
+	}
+
+	public bfs(startingNode: number) {
+		const visited: boolean[] = new Array(this.numberOfNodes).fill(false);
+		const queue: number[] = [];
+
+		visited[startingNode] = true;
+
+		queue.push(startingNode);
+
+		while (queue.length) {
+			const currentNode = queue.shift() as number;
+			console.log(currentNode);
+			const adjacentNodes = this.matrix[currentNode];
+
+			for (let i = 0; i < adjacentNodes.length; i++) {
+				const adjacentNode = adjacentNodes[i];
+
+				if (!visited[adjacentNode]) {
+					visited[adjacentNode] = true;
+					queue.push(adjacentNode);
+				}
+			}
+		}
+	}
 }
 
 export default Graph;
